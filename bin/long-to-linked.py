dcb24cf07c4c5abc1cd19936a4965db79f055dc9
Janet Li <janetxinli@gmail.com>
2020-01-10 09:00:55 -0800

    Add features for tigmint-long-cut

--- bin/long-to-linked.py
+++ bin/long-to-linked.py
@@ -14,9 +14,6 @@ def split_long_read(read_string, n=500):
 
 #print(split_long_read("abcdefghijklmnopqrs"))
 
-# Open output file for writing
-#out_file = open(sys.argv[2], 'w')
-
 # Load long ONT reads ** add here
 with open(sys.argv[1],'r') as long_reads:
     for i, line in enumerate(long_reads):
@@ -25,11 +22,5 @@ with open(sys.argv[1],'r') as long_reads:
         if (i + 1) % 2 == 0: # Line containing read in fasta file
             split_reads = split_long_read(line) # Split long read into a list of shorter reads
             for read in split_reads:
-#                out_file.write(">" + header + " BX:Z:" + str(i))
-#                print(header + " BX:Z:" + str(int((i+1)/2)))
-#                print(">BX:Z:" + str(int((i+1)/2)))
-                print(header.replace(" ", "") + " BX:Z:" + str(int((i+1)/2)))
-#                out_file.write(read)
+                print(header.replace(" ", "") + "_" + str(split_reads.index(read)) + " BX:Z:" + str(int((i+1)/2)))
                 print(read)
-            
-#out_file.close()
