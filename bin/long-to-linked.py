29b68a09ae263592841d17b8adbeb17880972198
Janet Li <janetxinli@gmail.com>
2020-03-10 10:56:54 -0700

    Add read_fasta to long-to-linked.py

--- bin/long-to-linked.py
+++ bin/long-to-linked.py
@@ -7,7 +7,9 @@ usage: gunzip -c [long_read_file.fq.gz] | python3 long-to-linked.py -l [cut leng
 '''
 
 import sys
+import gzip
 import argparse
+from read_fasta import read_fasta
 
 # Parse arguments
 def parse_arguments():
@@ -16,6 +18,9 @@ def parse_arguments():
     parser.add_argument( "-l", "--length", \
             metavar="cut length", dest="cut_length", default=500, type=int, \
             help="Length for pseudo-linked reads to be cut")
+    parser.add_argument("-r", "--reads", \
+            metavar="long read file", dest="long_reads", required=True, \
+            help="Long read file")
     return parser.parse_args()
 
 # Split long reads and output a list containing these reads
@@ -23,46 +28,22 @@ def split_long_read(read_string, length):
     return [read_string.strip()[0+i:l+i] for i in range(0, len(read_string), l)]
 
 def cut_reads(length):
-    
-    # Read first line to determine file type
-    line = sys.stdin.readline()
-#    print(line, file=sys.stdout)
+    """ 
+    Open long read file and print cut reads to stdout
+    """
 
-    # Fasta file type
-    # Load header of first fasta entry
-    if line[0] == ">":
-        header = line.strip().replace(" ", "_")
-        # Load remainder of reads
-        for i, line in enumerate(sys.stdin):
-            if (i + 1) % 2 == 0:
-                header = line.strip().replace(" ", "_")
-            if (i + 1) % 2 == 1: # Line containing read in fasta file
-                split_reads = split_long_read(line, length) # Split long read into a list of shorter reads
-                for read in split_reads:
-                    print(header + "_" + str(split_reads.index(read)) + " BX:Z:" + str(int((i+1)/2)), file=sys.stdout)
-                    print(read, file=sys.stdout)
-
-    # Fastq file type
-    # Load header of first fastq entry
-    elif line[0] == "@":
-        header = line.strip().replace(" ", "_")
-        fa_header = ">" + header[1:]
-#       print(header, file=sys.stdout)
-    # Load remainder of reads
-        for i, line in enumerate(sys.stdin):
-            if (i % 4) == 3:
-                header = line.strip().replace(" ", "_")
-                fa_header = ">" + header[1:]
-#               print(line, file=sys.stdout)
-            if (i % 4) == 0:
-                split_reads = split_long_read(line, length)
-                for read in split_reads:
-                    print(fa_header + "_" + str(split_reads.index(read)) + " BX:Z:" + str(int((i + 1) / 4)), file=sys.stdout)
-                    print(read, file=sys.stdout)
-
+    with gzip.open(ont, 'rt') as long_reads:
+        i = 0
+        for header, seq, _, _ in read_fasta(long_reads):
+            i += 1
+            split_reads = split_long_read(seq, length)
+            for read in split_reads:
+                print(">" + header + "_" + str(split_reads.index(read)) + " BX:Z:" + str(i), file=sys.stdout)
+                print(read, file=sys.stdout)
 
 if __name__ == "__main__":
     arguments = parse_arguments()
     l = arguments.cut_length
+    ont = arguments.long_reads
     cut_reads(l)
 
