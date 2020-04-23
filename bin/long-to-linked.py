6a7906b23aa531cf9ba0d5ee3c6e594c7efee3d5
Janet Li <janetxinli@gmail.com>
2020-04-22 18:14:28 -0700

    Add minimum molecule size parameter

--- bin/long-to-linked.py
+++ bin/long-to-linked.py
@@ -19,6 +19,9 @@ def parse_arguments():
     parser.add_argument("-r", "--reads", \
             metavar="long read file", dest="long_reads", required=True, \
             help="Long read file")
+    parser.add_argument("-m", "--min_size",\
+            metavar="minimum read size", dest="min_size", default=2000, type=int, \
+            help="Minimum read length to be considered a molecule")
     return parser.parse_args()
 
 # Split long reads and output a list containing these reads
@@ -28,7 +31,7 @@ def split_long_read(read_string, length):
     """
     return [read_string.strip()[0+i:l+i] for i in range(0, len(read_string), l)]
 
-def cut_reads(length, long_reads):
+def cut_reads(length, long_reads, min_size):
     """ 
     Open gzipped long read file. Print cut reads to stdout.
     """
@@ -36,14 +39,16 @@ def cut_reads(length, long_reads):
         i = 0
         for header, seq, _, _ in read_fasta(long_reads):
             i += 1
-            split_reads = split_long_read(seq, length)
-            for read in split_reads:
-                print(">" + header + "_" + str(split_reads.index(read)) + " BX:Z:" + str(i), file=sys.stdout)
-                print(read, file=sys.stdout)
+            if len(seq) >= min_size:
+                split_reads = split_long_read(seq, length)
+                for read in split_reads:
+                    print(">" + header + "_" + str(split_reads.index(read)) + " BX:Z:" + str(i), file=sys.stdout)
+                    print(read, file=sys.stdout)
 
 if __name__ == "__main__":
     arguments = parse_arguments()
     l = arguments.cut_length
     ont = arguments.long_reads
-    cut_reads(l, ont)
+    min_size = arguments.min_size
+    cut_reads(l, ont, min_size)
 
