9358510eba348bbb6470337f6a3f8e913a2be0be
Janet Li <janetxinli@gmail.com>
2020-04-28 17:18:48 -0700

    Minor edits

--- bin/long-to-linked.py
+++ bin/long-to-linked.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3 
 """
 Simulate linked reads from long ONT reads
-Usage: gunzip -c [long_read_file.fa.gz] | python3 long-to-linked.py -l [cut length] | gzip > file_name.fa.gz
+Usage: python3 long-to-linked.py -r reads.fa.gz | gzip > reads.cutlength.fa.gz
 """
 
 import sys
@@ -9,27 +9,11 @@ import gzip
 import argparse
 from read_fasta import read_fasta
 
-# Parse arguments
-def parse_arguments():
-    parser = argparse.ArgumentParser( \
-            description="Split long reads into pseudo-linked reads with a length of l.")
-    parser.add_argument( "-l", "--length", \
-            metavar="cut length", dest="cut_length", default=500, type=int, \
-            help="Length for pseudo-linked reads to be cut")
-    parser.add_argument("-r", "--reads", \
-            metavar="long read file", dest="long_reads", required=True, \
-            help="Long read file")
-    parser.add_argument("-m", "--min_size",\
-            metavar="minimum read size", dest="min_size", default=2000, type=int, \
-            help="Minimum read length to be considered a molecule")
-    return parser.parse_args()
-
-# Split long reads and output a list containing these reads
 def split_long_read(read_string, length):
     """
     Take an input read string and length l. Output a list containing substrings of read of length l.
     """
-    return [read_string.strip()[0+i:l+i] for i in range(0, len(read_string), l)]
+    return [read_string.strip()[0+i:length+i] for i in range(0, len(read_string), length)]
 
 def cut_reads(length, long_reads, min_size):
     """ 
@@ -45,10 +29,28 @@ def cut_reads(length, long_reads, min_size):
                     print(">" + header + "_" + str(split_reads.index(read)) + " BX:Z:" + str(i), file=sys.stdout)
                     print(read, file=sys.stdout)
 
-if __name__ == "__main__":
-    arguments = parse_arguments()
+def main():
+
+    # Parse arguments
+    parser = argparse.ArgumentParser( \
+            description="Split long reads into pseudo-linked reads with a length of l.")
+    parser.add_argument( "-l", "--length", \
+            metavar="cut length", dest="cut_length", default=500, type=int, \
+            help="Length for pseudo-linked reads to be cut")
+    parser.add_argument("-r", "--reads", \
+            metavar="long read file", dest="long_reads", required=True, \
+            help="Long read file")
+    parser.add_argument("-m", "--min_size",\
+            metavar="minimum read size", dest="min_size", default=2000, type=int, \
+            help="Minimum read length to be considered a molecule")
+
+    arguments = parser.parse_args()
+
     l = arguments.cut_length
     ont = arguments.long_reads
     min_size = arguments.min_size
-    cut_reads(l, ont, min_size)
 
+    cut_reads(l, ont, min_size)
+   
+if __name__ == "__main__":
+    main()
