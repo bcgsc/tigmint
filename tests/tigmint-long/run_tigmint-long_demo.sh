92d721085588b310d0b1143a0182bfae167c543c
Janet Li <janetxinli@gmail.com>
2020-04-30 09:46:54 -0700

    Change name from tigmint-long-cut to tigmint-long

--- tests/tigmint-long/run_tigmint-long_demo.sh
+++ tests/tigmint-long/run_tigmint-long_demo.sh
@@ -2,6 +2,6 @@
 
 echo "Running tigmint-make tigmint-long-cut..."
 
-../../bin/tigmint-make tigmint-long-cut draft=test_contig reads=test_longreads span=2
+../../bin/tigmint-make tigmint-long draft=test_contig reads=test_longreads span=2
 
 echo "Done! Compare your output files to those in the 'expected_outputs' directory to ensure that the run was successful."
