fef437da740e74416017dd11c3c11e85bb56ed7e
Janet Li <janetxinli@gmail.com>
2020-08-31 10:47:53 -0700

    Bump up version numbers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tigmint",
-    version="1.2.0",
+    version="1.2.1",
     author="Shaun Jackman",
     author_email="sjackman@gmail.com",
     description="Correct misassemblies using linked or long reads",
