087323349f578bb7b9cd1e8c2444092fd8ed9410
Janet Li <56139490+janetxinli@users.noreply.github.com>
2021-01-15 11:01:28 -0800

    Prepare for Release Version 1.2.2 (#43)
    * Allow genome size to be given in scientific notation
    
    * Update genome size parameter info
    
    * Bump up version numbers
    
    * Don't require genome_size argument
    
    * Calculate dist using only read lengths > min_size
    
    * Use 2 * cut length as lower limit for dist
    
    * Add time commands to long-to-linked and minimap2 steps for tigmint-long
    
    * use 1000 as lower bound for dist
    
    * create fastq file for cut reads
    
    * simplify get_genome_size
    
    * create argument for dist read length percentile
    
    * v1.2.2
    
    * update pipeline flow chart
    
    * change string formatting
    
    * change expected long-to-linked outputs to fastq
    
    * new expected output for long-to-linked
    
    * Use p50 for dist calculation
    
    * Update tests to use lower-bounded read length p50 as auto dist
    
    * Don't upgrade pip installations
    
    * Use python3.8 and add upgrade command back
    
    * Fix python paths
    
    * Update docstring
    
    Co-authored-by: Lauren Coombe <lauren.e.coombe@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tigmint",
-    version="1.2.1",
+    version="1.2.2",
     author="Shaun Jackman",
     author_email="sjackman@gmail.com",
     description="Correct misassemblies using linked or long reads",
