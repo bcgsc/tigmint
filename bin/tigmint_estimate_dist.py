64107b3e46e3f84a705282b40778551cde3ab51c
lcoombe <lauren.e.coombe@gmail.com>
2022-04-08 14:12:58 -0700

    Prepare for release v1.2.6

--- bin/tigmint_estimate_dist.py
+++ bin/tigmint_estimate_dist.py
@@ -44,7 +44,7 @@ def main():
                         required=False)
     parser.add_argument("-v", "--version",
                         action="version",
-                        version="tigmint_estimate_dist.py 1.2.5")
+                        version="tigmint_estimate_dist.py 1.2.6")
 
     args = parser.parse_args()
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tigmint",
-    version="1.2.5",
+    version="1.2.6",
     author="Shaun Jackman",
     author_email="sjackman@gmail.com",
     description="Correct misassemblies using linked or long reads",
