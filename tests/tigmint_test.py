c1fbdc7fead049a5cfb44a478235dd109bc2c993
lcoombe <lauren.e.coombe@gmail.com>
2026-02-04 14:02:52 -0800

    In test comparing alignments, only compare  first 11 and last columns
    - Extra tags with newer tool versions cause erroneous failures

--- tests/tigmint_test.py
+++ tests/tigmint_test.py
@@ -225,10 +225,11 @@ def test_pipeline(tigmint_pipeline):
                             stdout=subprocess.PIPE, universal_newlines=True)
     obs = subprocess.Popen(shlex.split("samtools view %s" % bam), stdout=subprocess.PIPE,
                             universal_newlines=True)
-    exp_bam = exp.communicate()[0].splitlines()
-    obs_bam = obs.communicate()[0].splitlines()
+    exp_bam = exp.communicate()[0].splitlines().split("\t")
+    obs_bam = obs.communicate()[0].splitlines().split("\t")
     for i, exp_alignment in enumerate(exp_bam):
-        assert exp_alignment == obs_bam[i]
+        assert exp_alignment[:11] == obs_bam[i][:11]
+        assert exp_alignment[-1] == obs_bam[-1]
     
     # Other readable files
     tigmint_outputs = ["test_contig.test_linkedreads.as0.65.nm5.molecule.size2000.dist50000.bed",
